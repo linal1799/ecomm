9e6fac771e0d49cf11119effde6e0a96969a04f7
linal1799 <linalshende17@gmail.com>
2022-08-25 14:50:49 +0530

    laravel admin template blading

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
